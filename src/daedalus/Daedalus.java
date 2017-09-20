24d98d5037a8c3c9d67ca7ca6f91b9b5ff0bea74
pixelrazor <darkria45@gmail.com>
2017-09-20 04:06:18 -0500

    Remove comments
    I also forgot to remove the default netbeans comments. woops.

--- src/daedalus/Daedalus.java
+++ src/daedalus/Daedalus.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package daedalus;
 
 import javafx.application.Application;
@@ -12,10 +7,6 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-/**
- *
- * @author Pixel
- */
 public class Daedalus extends Application {
     
     @Override
@@ -32,9 +23,6 @@ public class Daedalus extends Application {
         stage.show();
     }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         launch(args);
     }
